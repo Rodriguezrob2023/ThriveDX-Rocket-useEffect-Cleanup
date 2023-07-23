4ef0c78ecda64d5d23bc8020105f90b28a62aa06
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-07-23 12:47:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import RocketLauncher from './RocketLauncher'
 
 function App() {
   let [launch, setLaunch] = useState(true)
+  let [rocketName, setRocketName] = useState('Apollo')
+
+const handleChange =(e) => {
+  setRocketName(e.target.value)
+}
 
   return (
     <main>
@@ -11,6 +16,8 @@ function App() {
       <button onClick={() => setLaunch(!launch)}>
       { launch ? "Abort Launch" : "Start Launch" }
       </button>
+      {/* Changing the input will run the componet update*/}
+      <input type="text" placeholder="Rocket name" onChange={handleChange} />
       { launch ? <RocketLauncher /> : null }
     </main>
   );
